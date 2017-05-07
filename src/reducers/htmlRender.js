0081e9bc7ac039c3056500ae9bf6fbab362b29c4
Ryuichi Sakagami <shagamiiiii@gmail.com>
2017-05-07 22:54:24 +0900

    exchange to component

--- src/reducers/htmlRender.js
+++ src/reducers/htmlRender.js
@@ -4,8 +4,9 @@ const { HTML_RENDER } = actionTypes;
 
 function initialState() {
   return {
-    html_row: "",
-    css_row: ""
+    html_row: '',
+    css_row: "",
+    javascript_row: ""
   };
 };
 
@@ -17,12 +18,18 @@ function whenChangeCss(state, css_row) {
   return { ...state, css_row };
 }
 
+function whenChangeJS(state, javascript_row) {
+  return { ...state, javascript_row };
+}
+
 export default function htmlRender(state = initialState(), event) {
   switch (event.type) {
     case HTML_RENDER.CHANGE_HTML:
       return whenChangeHtml(state, event.newValue);
     case HTML_RENDER.CHANGE_CSS:
       return whenChangeCss(state, event.newValue);
+    case HTML_RENDER.CHANGE_JS:
+      return whenChangeJS(state, event.newValue);
     default:
       return state;
   }
